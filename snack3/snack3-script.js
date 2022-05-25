146087944695e2423284c438404f1a2a7db5363f
silviawolves <silviacanepa.w@gmail.com>
2022-05-25 15:38:12 +0200

    snack3 - completo

--- snack3/snack3-script.js
+++ snack3/snack3-script.js
@@ -1,7 +1,19 @@
 'use strict'
 
+//creo array per inserire i numeri e farne la somma + variabile per la somma
+const arrayNumeri = []
+let sommaArray = 0
 
+//creo il ciclo do-while per chiedere i numeri 5 volte e fare la somma
+do {
+    const numeroUtente = parseInt(prompt('Inserisci un numero!'))
+    arrayNumeri.push(numeroUtente)
+    sommaArray += numeroUtente
+} while (arrayNumeri.length < 5)
 
+//mostro contenuto array + somma di quel contenuto
+console.log(arrayNumeri)
+console.log(sommaArray)
 
 
 /*
