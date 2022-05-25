9175ac8dcd1269ced63507b81e8ac5b72b27a882
silviawolves <silviacanepa.w@gmail.com>
2022-05-25 14:16:21 +0200

    snack1 - completo

--- snack1/snack1-script.js
+++ snack1/snack1-script.js
@@ -1,11 +1,24 @@
 'use strict'
 
+//creo l'array vuoto dove finitanno i numeri inseriti dall'utente
+const arrayNumeri = []
 
+//creo una costante a 0 per la somma che devo operare sui numeri inseriti nell'array
+let sommaNumeri = 0
 
+//creo un ciclo while per chiedere i numeri all'utente finché la somma non è minore di 50
+do {
+    const numeriUtente = parseInt(prompt('Inserisci un numero!'))
+    sommaNumeri += numeriUtente
+    console.log(numeriUtente)
 
+        if (sommaNumeri < 50) {
+            arrayNumeri.push(numeriUtente)
+        }
+} while (sommaNumeri < 50 )
 
-
-
+console.log(sommaNumeri)
+console.log(arrayNumeri)
 
 
 /*
