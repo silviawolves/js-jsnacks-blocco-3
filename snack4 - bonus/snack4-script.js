481d87be92d81a2c14f2ddf7cb2aaede9393b8f2
silviawolves <silviacanepa.w@gmail.com>
2022-05-25 18:55:27 +0200

    imperfetto - completo

--- snack4 - bonus/snack4-script.js
+++ snack4 - bonus/snack4-script.js
@@ -16,7 +16,6 @@ do {
     }
 } while (invitoFesta = false)
 
-
 /*
 In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
 Interrompi il ciclo appena il nome è stato trovato.
