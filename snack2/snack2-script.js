781dd08505af588759d7c30c36dd46fdc43434d1
silviawolves <silviacanepa.w@gmail.com>
2022-05-26 10:09:25 +0200

    correzione snack2 - completo

--- snack2/snack2-script.js
+++ snack2/snack2-script.js
@@ -1,12 +1,30 @@
 'use strict'
 
 //faccio inserire un numero all'utente
-let numeroUtente = parseInt(prompt('Inserisci un numero!'))
+const numeroUtente = parseInt(prompt('Inserisci un numero!'))
 console.log(numeroUtente)
 
-let arrayNuovi = []
-let k = 0
+const arrayGenerati = []
 
+while (arrayGenerati.length < numeroUtente) {
+    const nuovoArray = []
+
+    while (nuovoArray.length < 10) {
+        const numeroCasuale = Math.floor(Math.random() * 100) + 1
+
+        if (!nuovoArray.includes(numeroCasuale)) {
+            nuovoArray.push(numeroCasuale)
+        }
+    }
+
+    arrayGenerati.push(nuovoArray)
+}
+console.log(arrayGenerati)
+
+
+/* come avevo fatto l'esercizio io
+
+let k = 0
 while (k < numeroUtente) {
     arrayNuovi === numeroUtente[k]
         for (let i = 0; i < 10; i++) {
@@ -14,7 +32,7 @@ while (k < numeroUtente) {
         }
     k++
     console.log(arrayNuovi)
-}
+}*/
 
 /*
 Fai inserire un numero, che chiameremo N, all’utente.
